6092d866fe377293cc197b42402507d68eddf9ea
Icey-0528 <92418802+Icey-0528@users.noreply.github.com>
2026-09-15 19:55:40 -0700

    dp_grid

--- run_recbole_cdr.py
+++ run_recbole_cdr.py
@@ -45,8 +45,8 @@ DATASET_CONFIG_DIR = (
 
 COMPUTE_DEVICE = 'auto'
 GPU_ID = '0'
-DP_SEEDS = (2022, 2023, 2024)
-DP_EPSILONS = (0.1, 100.0)
+DP_SEEDS = (2024)
+DP_EPSILONS = (5.0)
 
 
 def resolve_dataset_preset(dataset_preset, dataset_root):
